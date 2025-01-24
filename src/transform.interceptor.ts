9019c9e941c7f6492b5e5f243db482bba2e33237
den-bulaev <denisbulaev7@gmail.com>
2025-01-24 13:33:58 +0200

    global request shape transform and validationpipe were added

---
+++ src/transform.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler) {
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: context.switchToHttp().getResponse().statusCode,
+        message: 'Request successful',
+        data,
+      })),
+    );
+  }
+}
