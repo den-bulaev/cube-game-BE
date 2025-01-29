defa111fb3b802f58e248316a475c7893d7c801a
den-bulaev <denisbulaev7@gmail.com>
2025-01-29 19:05:59 +0200

    logs updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,14 @@ import { configValidationSchema } from './config.schema';
       useFactory: async (configService: ConfigService) => {
         const isProduction = configService.get('NODE_ENV') === 'prod';
         console.log('NODE_ENV', configService.get('NODE_ENV'));
-        console.log('PGHOST', configService.get('NODE_ENV'));
-        console.log('PGPORT', configService.get('NODE_ENV'));
-        console.log('POSTGRES_USER', configService.get('NODE_ENV'));
-        console.log('POSTGRES_PASSWORD', configService.get('NODE_ENV'));
-        console.log('POSTGRES_DB', configService.get('NODE_ENV'));
+        console.log('PGHOST', configService.get('PGHOST'));
+        console.log('PGPORT', configService.get('PGPORT'));
+        console.log('POSTGRES_USER', configService.get('POSTGRES_USER'));
+        console.log(
+          'POSTGRES_PASSWORD',
+          configService.get('POSTGRES_PASSWORD'),
+        );
+        console.log('POSTGRES_DB', configService.get('POSTGRES_DB'));
 
         return {
           ssl: isProduction,
