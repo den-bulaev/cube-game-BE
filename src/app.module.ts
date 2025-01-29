5f20f041ea6a6bbf74ae4e6d4f3d8e3e994d96c3
den-bulaev <denisbulaev7@gmail.com>
2025-01-29 20:01:59 +0200

    log added

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ console.log('POSTGRES_USER', process.env.POSTGRES_USER);
 console.log('POSTGRES_PASSWORD', process.env.POSTGRES_PASSWORD);
 console.log('POSTGRES_DB', process.env.POSTGRES_DB);
 console.log('JWT_SECRET', process.env.JWT_SECRET);
+console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
 
 @Module({
   imports: [
