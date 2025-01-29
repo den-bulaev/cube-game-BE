99323ff3c283c38137ed6f117019617a21fbb755
den-bulaev <denisbulaev7@gmail.com>
2025-01-29 15:18:34 +0200

    env variables updated

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 
-  logger.log(`App listening on port ${3000}`);
+  logger.log(`App listening on port ${process.env.PORT}`);
 }
 bootstrap();
