c78d1c3b78567374f22f674ff3c12acefe17e5b5
den-bulaev <denisbulaev7@gmail.com>
2025-01-30 19:25:40 +0200

    deployment to railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,46 +1,45 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { ScoreModule } from './score/score.module';
 import { configValidationSchema } from './config.schema';
 
 const isProduction = process.env.NODE_ENV === 'prod';
-console.log('NODE_ENV', process.env.NODE_ENV);
-console.log('PGHOST', process.env.PGHOST);
-console.log('PGPORT', process.env.PGPORT);
-console.log('POSTGRES_USER', process.env.POSTGRES_USER);
-console.log('POSTGRES_PASSWORD', process.env.POSTGRES_PASSWORD);
-console.log('POSTGRES_DB', process.env.POSTGRES_DB);
-console.log('JWT_SECRET', process.env.JWT_SECRET);
-console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath:
-        process.env.NODE_ENV === 'prod'
-          ? []
-          : [`.env.stage.${process.env.NODE_ENV}`],
+      envFilePath: isProduction ? [] : [`.env.stage.${process.env.NODE_ENV}`],
       validationSchema: configValidationSchema,
     }),
     AuthModule,
     ScoreModule,
-    TypeOrmModule.forRoot({
-      ssl: isProduction,
-      extra: {
-        ssl: isProduction ? { rejectUnauthorized: false } : null,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
+
+        console.log('***PGHOST***', configService.get('PGHOST'));
+
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: !isProduction,
+          host: configService.get('PGHOST'),
+          port: configService.get('PGPORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          url: process.env.DATABASE_URL,
+        };
       },
-      type: 'postgres',
-      host: process.env.PGHOST,
-      port: Number(process.env.PGPORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: !isProduction,
-      url: process.env.DATABASE_URL,
     }),
   ],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,13 +9,19 @@ import { User } from './user.entity';
 import { JwtStrategy } from './jwt.strategy';
 import { Score } from 'src/score/score.entity';
 import { TopScore } from 'src/score/topScore.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: 3600 },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: 3600 },
+      }),
     }),
     TypeOrmModule.forFeature([User, Score, TopScore]),
   ],
